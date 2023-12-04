c94f49a7bbbf6247e8039217fc5b98913ec8ba05
ManavSunka9 <manavsunka@gmail.com>
2023-12-04 22:54:44 +0000

    add

---
+++ src/recipes.js
@@ -0,0 +1,29 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Butter Chicken",
+    price: 12.99,
+    image: "https://cafedelites.com/wp-content/uploads/2019/01/Butter-Chicken-IMAGE-64.jpg",
+    description:
+      "Butter chicken, traditionally known as murgh makhani, is an Indian dish originating in Delhi. It is a type of curry made from chicken with a spiced tomato and butter sauce.",
+  },
+  {
+    id: 2,
+    title: "Egg Fried Rice",
+    price: 5.99,
+    image: "https://takestwoeggs.com/wp-content/uploads/2023/03/Egg-Fried-Rice-Takestwoeggs-Final-Photography-sq.jpg",
+    description:
+      "Fried rice originated in Yangzhou in the eastern Jiangsu province of China and was a favorite of Emperor Yang of the Sui Dynasty.",
+  },
+  {
+    id: 3,
+    title: "Shawarma",
+    price: 4.78,
+    image: "https://theperfecttide.com/wp-content/uploads/2022/06/DSC_5536-1-scaled.jpg",
+    description:
+      "Shawarma is a Middle Eastern dish that originated in the Levant region of the Arab world during the Ottoman Empire, consisting of meat cut into thin slices, stacked in an inverted cone, and roasted on a slowly turning vertical rotisserie or spit.",
+  },
+
+];
+
+export default recipes;
